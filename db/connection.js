6e1a086bf4c3ff45dfc5d03d8764e4a05b94d7a3
Nim Man <nimman@192.168.1.170>
2024-01-16 16:38:11 +0000

    additional commit to test render

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const ENV = process.env.NODE_ENV || 'dev';
 
 const config = {}
 
-if (ENV = "production") {
+if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL
   config.max = 2
 }
@@ -17,5 +17,4 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-
-module.exports = new Pool();
+module.exports = new Pool(config);
